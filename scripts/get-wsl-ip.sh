d4a5b353763e548bddf80566035e328913ae8ff7
David Kakes <david.kakes@capgemini.com>
2025-01-31 12:59:23 +0000

    Update ip script

--- scripts/get-wsl-ip.sh
+++ scripts/get-wsl-ip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ip route show | grep eth0 | grep -v via | cut -d' ' -f1 | sed 's/0\/20/1/' | tr -d '\n'
+mkdir -p .vscode
+ip route show | grep eth0 | grep -v via | cut -d' ' -f1 | sed 's/0\/20/1/' | tr -d '\n' > .vscode/wsl-ip
